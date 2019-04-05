1483f647bab78f7104b509c4b14b8b9e346a41db
ks2019675073 <48661228+HwangYeLim@users.noreply.github.com>
2019-04-05 18:25:02 +0900

    Add files via upload

---
+++ 문자 데이터 출력.py
@@ -0,0 +1,21 @@
+print("python")
+print('python')
+print("""python""")
+print('''파이썬''')
+print("''파이썬''")
+
+print("안녕 python")
+print("안녕 + python")
+print("안녕"+"python")
+print("안녕"+'python')
+print("안녕"+" "+"python")
+
+print("10+20")
+print("10"+"20")
+
+print("파이썬*10")
+print("파이썬"*10)
+
+
+print("print('파이썬'*'10')은 출력되지 않는다")
+print("('괄호 안에 같은 따옴표가 입력되면 출력되지 않는다')")
---
+++ 터틀 그래픽.py
@@ -0,0 +1,5 @@
+import turtle
+turtle.shape("turtle")
+turtle.write(turtle.position())
+turtle.forward(50)
+turtle.write(turtle.position())
